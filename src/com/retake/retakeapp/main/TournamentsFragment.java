41ad1491281a2fbd8507b19fb0d8ac8e601e064d
iulianchis <iulian.chis@gmail.com>
2015-04-07 05:35:18 +0300

    Tournaments...

--- src/com/retake/retakeapp/main/TournamentsFragment.java
+++ src/com/retake/retakeapp/main/TournamentsFragment.java
@@ -8,9 +8,11 @@ import android.view.ViewGroup;
 import com.example.retakeapp.R;
 import com.retake.retakeapp.base.BaseFragment;
 import com.retake.retakeapp.base.BaseModel;
+import com.retake.retakeapp.utils.*;
 
 public class TournamentsFragment extends BaseFragment {
-
+	ImageMap mImageMap;
+	
 	public TournamentsFragment() {
 	}
 
@@ -21,6 +23,29 @@ public class TournamentsFragment extends BaseFragment {
 		View rootView = inflater.inflate(R.layout.fragment_tournaments,
 				container, false);
 
+		 // find the image map in the view
+		mImageMap = (ImageMap) rootView.findViewById(R.id.map);
+	   
+        
+        // add a click handler to react when areas are tapped
+        mImageMap.addOnImageMapClickedHandler(new ImageMap.OnImageMapClickedHandler()
+        {
+			@Override
+			public void onImageMapClicked(int id, ImageMap imageMap)
+			{
+				// when the area is tapped, show the name in a 
+				// text bubble
+				imageMap.showBubble(id);
+			}
+
+			@Override
+			public void onBubbleClicked(int id)
+			{
+				// react to info bubble for area being tapped
+			}
+		});
+        mImageMap.setImageResource(R.drawable.ladder);
+        
 		return rootView;
 	}
 
